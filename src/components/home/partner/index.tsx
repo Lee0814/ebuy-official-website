12eafba7de128a77326f4410b043cf15bcd12900
张旭东 <2219454275@qq.com>
2023-04-23 17:27:50 +0800

    1111

--- src/components/home/partner/index.tsx
+++ src/components/home/partner/index.tsx
@@ -1,89 +1,137 @@
+import { rAFWithControl } from "@/utils";
+import { useInViewport } from "ahooks";
 import Image from "next/image";
-import { memo, useEffect, useRef } from "react";
+import { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 import styles from "./styles.module.scss";
 
-import p11 from "./images/logo-1-1.png";
-import p12 from "./images/logo-1-2.png";
-import p13 from "./images/logo-1-3.png";
-import p14 from "./images/logo-1-4.png";
-import p15 from "./images/logo-1-5.png";
-import p16 from "./images/logo-1-6.png";
-import p17 from "./images/logo-1-7.png";
+import logo11 from "./images/logo-1-1.png";
+import logo12 from "./images/logo-1-2.png";
+import logo13 from "./images/logo-1-3.png";
+import logo14 from "./images/logo-1-4.png";
+import logo15 from "./images/logo-1-5.png";
+import logo16 from "./images/logo-1-6.png";
+import logo17 from "./images/logo-1-7.png";
 
-import p21 from "./images/logo-2-1.png";
-import p22 from "./images/logo-2-2.png";
-import p23 from "./images/logo-2-3.png";
-import p24 from "./images/logo-2-4.png";
-import p25 from "./images/logo-2-5.png";
-import p26 from "./images/logo-2-6.png";
-import p27 from "./images/logo-2-7.png";
+import logo21 from "./images/logo-2-1.png";
+import logo22 from "./images/logo-2-2.png";
+import logo23 from "./images/logo-2-3.png";
+import logo24 from "./images/logo-2-4.png";
+import logo25 from "./images/logo-2-5.png";
+import logo26 from "./images/logo-2-6.png";
+import logo27 from "./images/logo-2-7.png";
 
-import p31 from "./images/logo-3-1.png";
-import p32 from "./images/logo-3-2.png";
-import p33 from "./images/logo-3-3.png";
-import p34 from "./images/logo-3-4.png";
-import p35 from "./images/logo-3-5.png";
-import p36 from "./images/logo-3-6.png";
-import p37 from "./images/logo-3-7.png";
+import logo31 from "./images/logo-3-1.png";
+import logo32 from "./images/logo-3-2.png";
+import logo33 from "./images/logo-3-3.png";
+import logo34 from "./images/logo-3-4.png";
+import logo35 from "./images/logo-3-5.png";
+import logo36 from "./images/logo-3-6.png";
+import logo37 from "./images/logo-3-7.png";
 
-import p41 from "./images/logo-4-1.png";
-import p42 from "./images/logo-4-2.png";
-import p43 from "./images/logo-4-3.png";
-import p44 from "./images/logo-4-4.png";
-import p45 from "./images/logo-4-5.png";
-import p46 from "./images/logo-4-6.png";
-import p47 from "./images/logo-4-7.png";
+import logo41 from "./images/logo-4-1.png";
+import logo42 from "./images/logo-4-2.png";
+import logo43 from "./images/logo-4-3.png";
+import logo44 from "./images/logo-4-4.png";
+import logo45 from "./images/logo-4-5.png";
+import logo46 from "./images/logo-4-6.png";
+import logo47 from "./images/logo-4-7.png";
 
-import p51 from "./images/logo-5-1.png";
-import p52 from "./images/logo-5-2.png";
-import p53 from "./images/logo-5-3.png";
-import p54 from "./images/logo-5-4.png";
-import p55 from "./images/logo-5-5.png";
-import p56 from "./images/logo-5-6.png";
-import p57 from "./images/logo-5-7.png";
+import logo51 from "./images/logo-5-1.png";
+import logo52 from "./images/logo-5-2.png";
+import logo53 from "./images/logo-5-3.png";
+import logo54 from "./images/logo-5-4.png";
+import logo55 from "./images/logo-5-5.png";
+import logo56 from "./images/logo-5-6.png";
+import logo57 from "./images/logo-5-7.png";
 
-const partnerLogos = [
-  [p11, p21, p31, p41, p51],
-  [p12, p22, p32, p42, p52],
-  [p13, p23, p33, p43, p53],
-  [p14, p24, p34, p44, p54],
-  [p15, p25, p35, p45, p55],
-  [p16, p26, p36, p46, p56],
-  [p17, p27, p37, p47, p57],
+const logos = [
+  [logo11, logo21, logo31, logo41, logo51],
+  [logo12, logo22, logo32, logo42, logo52],
+  [logo13, logo23, logo33, logo43, logo53],
+  [logo14, logo24, logo34, logo44, logo54],
+  [logo15, logo25, logo35, logo45, logo55],
+  [logo16, logo26, logo36, logo46, logo56],
+  [logo17, logo27, logo37, logo47, logo57],
+  [logo11, logo21, logo31, logo41, logo51],
+  [logo12, logo22, logo32, logo42, logo52],
+  [logo13, logo23, logo33, logo43, logo53],
+  [logo14, logo24, logo34, logo44, logo54],
+  [logo15, logo25, logo35, logo45, logo55],
+  [logo16, logo26, logo36, logo46, logo56],
+  [logo17, logo27, logo37, logo47, logo57],
 ];
 
 export const Partner = memo(() => {
-  const dynamicDom = useRef<HTMLDivElement>(null);
-  let node: Node;
-  useEffect(() => {
-    let styleObj = dynamicDom.current?.style!;
-    let transitionX = 0;
+  // 滚动的偏移量
+  const [offset, setOffset] = useState(0);
 
-    let isAdd: number = 0;
-    const moveFn = () => {
-      styleObj.transform = `translateX(-${(transitionX += 1)}px)`;
+  // 所有的列
+  const [cols, setCols] = useState(
+    logos.map((_logos, i) => (
+      <div key={`partner-logo-${i}`}>
+        {_logos.map((src, j) => (
+          <div
+            key={`partner-logo-${i}-${j}`}
+            className="flex h-[144px] items-center justify-center"
+          >
+            <Image
+              src={src}
+              alt={`partner logo ${i} ${j}`}
+              className="max-h-[120px] max-w-[120px]"
+            />
+          </div>
+        ))}
+      </div>
+    ))
+  );
 
-      // if (
-      //   transitionX >
-      //   dynamicDom.current!.firstElementChild!.scrollWidth + 20
-      // ) {
-      //   dynamicDom.current!.appendChild(dynamicDom.current!.firstElementChild!);
-      // }
-
-      isAdd = requestAnimationFrame(moveFn);
-    };
-
-    if (!isAdd) {
-      moveFn();
-    }
+  // 控制动画的开始和结束
+  const partnerRef = useRef<HTMLDivElement>(null);
+  const [inViewport, ratio] = useInViewport(partnerRef, {
+    threshold: [0, 0.25, 0.5, 0.75, 1],
   });
+  const animation = useMemo(
+    () =>
+      rAFWithControl(60, () => {
+        setOffset((offset) => {
+          return offset + 1;
+        });
+      }),
+    []
+  );
+  const startAnimation = useCallback(() => {
+    animation.start();
+  }, [animation]);
+  const stopAnimation = useCallback(() => {
+    animation.stop();
+  }, [animation]);
+  useEffect(() => {
+    if (!inViewport || !ratio) return stopAnimation();
+    if (inViewport && ratio > 0.35) {
+      startAnimation();
+    } else {
+      stopAnimation();
+    }
+  }, [inViewport, ratio]);
+  useEffect(() => {
+    if (offset + 1 >= partnerRef.current!.clientWidth / 7) {
+      setCols((cols) => {
+        const _cols = [...cols];
+        const _col = _cols.shift();
+        _cols.push(_col!);
+        return _cols;
+      });
+      setOffset(0);
+    }
+  }, [offset]);
+  useEffect(() => stopAnimation, []);
 
   return (
     <section className="bg-white pt-[72px]">
       <div className="ebuy-container text-center">
         <span className="text-[44px] text-[#333333] md:text-[64px]">EBUY</span>
-        <span className="text-[30px] md:text-[50px]"> Grows with You</span>
+        <span className="text-[30px] md:text-[50px]">Grows with You</span>
         <div className="text-center text-[20px] leading-[44px] md:text-[28px]">
           As a trusted partner, we serve more than 1000 F&B businesses such as
           Haidilao Hot Pot, Putien, Wen Jia Bao and A Kitchen. Since the release
@@ -95,52 +143,21 @@ export const Partner = memo(() => {
           business efficiency.
         </div>
         <div className="relative flex flex-col py-[100px]">
-          <div className={styles.leftMist}></div>
-          <div className={styles.rightMist}></div>
+          <div className={`${styles.mist} left-0 bg-gradient-to-r`}></div>
           {/* 视口 */}
-          <div className="h-full overflow-x-hidden">
+          <div className="w-full overflow-x-hidden">
             {/* 滚动块 */}
-            {/* 单个图片块 */}
             <div
-              ref={dynamicDom}
-              className={`flex w-[2880px] justify-between  ${styles.ani}`}
+              ref={partnerRef}
+              className="grid w-full max-w-full auto-cols-[14.285714%] grid-flow-col justify-between"
+              style={{
+                transform: `translateX(-${offset}px)`,
+              }}
             >
-              {partnerLogos.map((column, index) => (
-                <div key={index}>
-                  {column.map((src, index) => (
-                    <div
-                      key={index}
-                      className="flex h-[144px] items-center justify-center"
-                    >
-                      <Image
-                        className="max-h-[120px] w-[120px]"
-                        src={src}
-                        alt=""
-                      />
-                    </div>
-                  ))}
-                </div>
-              ))}
-              {/* {单个滚动块} */}
-              <div></div>
-              {partnerLogos.map((column, index) => (
-                <div key={index}>
-                  {column.map((src, index) => (
-                    <div
-                      key={index}
-                      className="flex h-[144px] items-center justify-center"
-                    >
-                      <Image
-                        className="max-h-[120px] w-[120px]"
-                        src={src}
-                        alt=""
-                      />
-                    </div>
-                  ))}
-                </div>
-              ))}
+              {cols}
             </div>
           </div>
+          <div className={`${styles.mist} right-0 bg-gradient-to-l`}></div>
         </div>
       </div>
     </section>
