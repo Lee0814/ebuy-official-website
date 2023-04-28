7828d74f7391c88b411afb59550b714414f88bee
Lee0814 <729844336@qq.com>
2023-04-28 18:41:26 +0800

    11

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/ebuy-official-website",
   reactStrictMode: true,
   images: {
     unoptimized: true,
