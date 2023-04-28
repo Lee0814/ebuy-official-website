99de55b579a3ae3fb6252e31a2acf248922171c2
Lee0814 <729844336@qq.com>
2023-04-28 16:28:40 +0800

    11

--- src/components/home/evaluation/index.tsx
+++ src/components/home/evaluation/index.tsx
@@ -1,37 +1,44 @@
 import { useI18n } from "@/hooks";
+import { rAFWithControl } from "@/utils";
+import { useInViewport } from "ahooks";
 import classNames from "classnames";
-import { memo, useEffect, useRef } from "react";
+import { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { ECard } from "./evaluatCard";
 export const Evaluation = memo(() => {
   const dom = useRef<HTMLDivElement>(null);
   const t = useI18n("home");
   useEffect(() => {});
   const evaluations = [
-    {
-      name: "Hai Di Lao Dining Pte. Ltd",
-      desc: "Hai Di Lao Mian Guan 2",
-      text: "The meat is fresh and just right. Surveyor carefully read the remarks, the work is in place.",
-      img: "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngcb45e33c055d2e6fd779884efdfcd0fe8f1fe7b32c7d8a00272a0ef67872b6c3",
-    },
-    {
-      name: "H&J TIBET PTE. LTD",
-      desc: "Miss Tang 14-18",
-      text: "Quickly put the little brother service in place, I fill in the wrong address, the little brother phone contact soon to me. Thank you very much!",
-      img: "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng98a71cc90415baa63400b2cb3f34d911c94531f43a08fc1cdb5ea88bdf2b904d",
-    },
-    {
-      name: "ASHION SPICY HOT POT",
-      desc: "Ashion Spicy Hot Pot 21",
-      text: "So night, brother hard, fast delivery. Thank you",
-      img: "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng339ecc6429bcff7c0053b1ecd50d5e8f1252077115b7cb7b9863d62c410248fd",
-    },
-    {
-      name: "Gu Xiang Ji Pte Ltd",
-      desc: "Da Yan 118",
-      text: "This apple is better than Aksu in New Zealand! Crispy and sweet! The ugliest apple I've ever seen and the best apple I've ever eaten",
-      img: "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng9e0fcf8cbdb8d1c74bc9ebb4f7cfb93a3f51fd86c191271129233f1aac7e3bf8",
-    },
+    "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPngcb45e33c055d2e6fd779884efdfcd0fe8f1fe7b32c7d8a00272a0ef67872b6c3",
+
+    "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng98a71cc90415baa63400b2cb3f34d911c94531f43a08fc1cdb5ea88bdf2b904d",
+
+    "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng339ecc6429bcff7c0053b1ecd50d5e8f1252077115b7cb7b9863d62c410248fd",
+
+    "https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng9e0fcf8cbdb8d1c74bc9ebb4f7cfb93a3f51fd86c191271129233f1aac7e3bf8",
   ];
+  const evalutionProps: Array<
+    Array<{
+      name: string;
+      desc: string;
+      text: string;
+      img: string;
+    }>
+  > = [];
+  for (let i = 0; i < 16; i++) {
+    if (!evalutionProps[Math.floor(i / 2)])
+      evalutionProps[Math.floor(i / 2)] = [];
+    evalutionProps[Math.floor(i / 2)].push({
+      name: t(`evaluation-${i + 1}-customer-company` as any),
+      desc: t(`evaluation-${i + 1}-customer-people` as any),
+      text: t(`evaluation-${i + 1}-customer-text` as any),
+      // img: evaluations[Math.floor(Math.random() * 4)],
+      img: evaluations[0],
+    });
+  }
+  //偏移逻辑
+  // 滚动的偏移量
+  const [offset, setOffset] = useState(0);
 
   // 所有的列
   const [cols, setCols] = useState(
@@ -47,10 +54,55 @@ export const Evaluation = memo(() => {
     ))
   );
 
+  // 控制动画的开始和结束
+  const partnerRef = useRef<HTMLDivElement>(null);
+  const [inViewport, ratio] = useInViewport(partnerRef, {
+    threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+  });
+  const animation = useMemo(
+    () =>
+      rAFWithControl(60, () => {
+        setOffset((offset) => {
+          return offset + 1;
+        });
+      }),
+    []
+  );
+  const startAnimation = useCallback(() => {
+    animation.start();
+  }, [animation]);
+  const stopAnimation = useCallback(() => {
+    animation.stop();
+  }, [animation]);
+  useEffect(() => {
+    if (!inViewport || !ratio) return stopAnimation();
+    if (inViewport && ratio > 0.35) {
+      startAnimation();
+    } else {
+      stopAnimation();
+    }
+  }, [inViewport, ratio]);
+  useEffect(() => {
+    if (offset + 1 >= partnerRef.current!.firstElementChild!.clientWidth) {
+      setCols((cols) => {
+        const _cols = [...cols];
+        const _col = _cols.shift();
+        _cols.push(_col!);
+        return _cols;
+      });
+      setOffset(0);
+    }
+  }, [offset]);
+  useEffect(() => stopAnimation, []);
   return (
-    <section ref={dom} className={classNames("w-full py-[45px] md:pb-[56px]")}>
+    <section
+      ref={dom}
+      className={classNames(
+        "w-full bg-[#fbfbfb] py-[45px]  md:pb-[100px] md:pt-[72px]"
+      )}
+    >
       {/* 主体 */}
-      <div className={classNames("ebuy-container")}>
+      <div className={classNames("ebuy-container ")}>
         {/* 标题 */}
         <div
           className={classNames(
@@ -70,32 +122,10 @@ export const Evaluation = memo(() => {
           </span>
           {t("evaluation-description-3")}
         </div>
-        {/* 评价 */}
-        {/* <div className={classNames("ebuy-container  col-start-1 col-end-25")}>
-          <div className={classNames("mr-[46px] flex justify-between")}>
-
-            <ECard eva={evaluations[0]} />
-            <ECard eva={evaluations[1]} />
-          </div>
-          <div
-            className={classNames("ml-[46px] mt-[78px] flex justify-between")}
-          >
-            <ECard eva={evaluations[2]} />
-            <ECard eva={evaluations[3]} />
-          </div>
-        </div> */}
-
-        {/* 卡片 */}
+        {/* 固定窗口 */}
         <div
           className={classNames(
-            "col-start-1 col-end-25 mb-[52px] pr-[20px] md:col-end-12"
-          )}
-        >
-          <ECard eva={evaluations[0]} />
-        </div>
-        <div
-          className={classNames(
-            "col-start-1 col-end-25 md:col-start-12 md:col-end-25 md:pl-[46px]"
+            "col-start-1 col-end-25 w-full overflow-x-hidden "
           )}
         >
           {/* 超出部分 */}
